9ce3f6954fad761dd9c0d981f58b372c34bd2e86
anita <slater.anita@gmail.com>
2018-03-25 17:40:46 -0400

    [proj1]Got half of the nested recursion down. Made a method that prints out the operands.

--- Visitor.java
+++ Visitor.java
@@ -1,5 +1,5 @@
 //@formatter:off
-import java.util.Queue;
+import java.util.Iterator;import java.util.List;import java.util.Queue;
 
 public class Visitor
 {
@@ -18,12 +18,67 @@ public class Visitor
           //  Visit(base);
         //}
     }
-    static void printList(Queue<Node> listOfNodes)
+    static void Visit(Queue<Node> listOfNodes)
+    {
+      System.out.println("--------------");
+      Iterator <Node> iter = listOfNodes.iterator();
+      Visitor.parseNodes(listOfNodes, iter);
+      System.out.println("--------------");
+      Visitor.uglyCode(listOfNodes);
+
+    }
+    static void parseNodes(Queue<Node> listOfNodes, Iterator <Node> iter)
+    {
+        if( iter.hasNext())
+        {
+            Queue<Node> nodeChildren = null;
+            Iterator <Node> childIter = null;
+
+            Node curNode = iter.next();
+
+            // int hasChildren = 0;
+            if( curNode.GetType() == 'w')
+            {
+                nodeChildren = ((WhileNode)curNode).conditions.children;
+                childIter    = nodeChildren.iterator();
+                 System.out.println("THIS IS A WHILE NODE AND THE FOLLOWING " +
+                        "ARE ITS CHILDREN");
+
+                parseOperands(nodeChildren, childIter);
+
+            }
+            System.out.println( "## "+curNode);
+            parseNodes(listOfNodes, iter);
+        }
+    }
+    public static void parseOperands(Queue <Node> nodeChildren,
+                                     Iterator<Node> childIter)
+    {
+        if(childIter != null && childIter.hasNext())
+        {
+            Node curNode = childIter.next();
+            //Node condition = nodeChildren.element();
+
+            Operand reg1 = ((TwoOperandNode)curNode).operand1;
+            Operand reg2 = ((TwoOperandNode)curNode).operand2;
+
+            printRegs(reg1,reg2,curNode);
+            parseOperands(nodeChildren,childIter);
+        }
+    }
+    public static void printRegs(Operand reg1, Operand reg2,
+                                Node oper)
+    {
+        System.out.print("V_pR: R"+((RegisterOperand)reg1).GetRegister());
+        System.out.print(" "+oper.GetType());
+        System.out.println(" R"+((RegisterOperand)reg2).GetRegister());
+    }
+    public static void uglyCode(Queue<Node> listOfNodes)
     {
         for ( Node item : listOfNodes )
-        {
-            System.out.println("V_pL: "+item+"----"+item.GetType());
-            if(item.GetType() == 'w')
+         {
+             System.out.println("V_pL: "+item+"----"+item.GetType());
+             if(item.GetType() == 'w')
             {
                 //////////////////////
                 // whilenode
@@ -33,11 +88,7 @@ public class Visitor
                     Operand reg1 = ((TwoOperandNode)condition).operand1;
                     Operand reg2 = ((TwoOperandNode)condition).operand2;
 
-                    System.out.print("V_pL_if_f: R"+((RegisterOperand)reg1)
-                            .GetRegister());
-                    System.out.print("  "+condition.GetType());
-                    System.out.println(" R"+((RegisterOperand)reg2)
-                            .GetRegister());
+                    printRegs(reg1,reg2,condition);
                 }
                 for ( Node condition: ((WhileNode)item).statements
                     .children )
@@ -47,11 +98,7 @@ public class Visitor
 
                     if( reg1.GetType() != 'A' && reg2.GetType() != 'A')
                     {
-                        System.out.print("V_pL_if_f: R"+((RegisterOperand)reg1)
-                            .GetRegister());
-                        System.out.print("  "+condition.GetType());
-                        System.out.println(" R"+((RegisterOperand)reg2)
-                            .GetRegister());
+                        printRegs(reg1,reg2,condition);
                     }
 
                 }
@@ -59,10 +106,11 @@ public class Visitor
                 //System.out.println("V_pl_if1: "+((WhileNode)item).statements
                 //    .children);
             }
+         }
 
-        }
     }
 }
+
       /*  WhileNode myWhile;
       for (Node item : givenNodes)
       {
