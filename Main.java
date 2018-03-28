0b57f043eb28b4a3cdc88ec093c54fa2bbb56bb6
anita <slater.anita@gmail.com>
2018-03-28 13:09:50 -0400

    [proj1] creating a better test case for main, reverting visitor back...

--- Main.java
+++ Main.java
@@ -16,20 +16,23 @@ public class Main {
 
     Queue<Node> subs = new LinkedList<Node>();
     subs.add(new TwoOperandNode('-',new RegisterOperand(0),new RegisterOperand(1)));
-    subs.add(new TwoOperandNode('-',new RegisterOperand(1),new RegisterOperand(2)));
-    subs.add(new TwoOperandNode('-',new RegisterOperand(2),new RegisterOperand(3)));
-    subs.add(new TwoOperandNode('-',new RegisterOperand(3),new RegisterOperand(4)));
-    subs.add(new TwoOperandNode('-',new RegisterOperand(4),new RegisterOperand(5)));
     subs.add(new TwoOperandNode('-',new AddressOperand(1000),new AddressOperand(2000)));
 
+    Queue<Node> wyle = new LinkedList<Node>();
+    wyle.add(new TwoOperandNode('-',new RegisterOperand(12),new RegisterOperand(13)));
+    wyle.add(new TwoOperandNode('-',new RegisterOperand(13),new RegisterOperand(14)));
+    wyle.add(new TwoOperandNode('-',new RegisterOperand(14),new RegisterOperand(15)));
+    wyle.add(new WhileNode(new StatementsNode(adds),new StatementsNode(subs)));
+
     Queue<Node> baseNodes = new LinkedList<Node>();
-    baseNodes.add(new TwoOperandNode('-',new AddressOperand(3333),new AddressOperand(4444)));
-    baseNodes.add(new WhileNode(new StatementsNode(adds),new StatementsNode(subs)));
+    baseNodes.add(new TwoOperandNode('-',new RegisterOperand(420),new RegisterOperand(69)));
+    baseNodes.add(new TwoOperandNode('-',new AddressOperand(1000),new AddressOperand(2000)));
+    baseNodes.add(new WhileNode(new StatementsNode(wyle),new StatementsNode(adds)));
     Node base = new StatementsNode(baseNodes);
 
     Visitor.Visit(base);
     System.out.println("Second visitor call---------------------");
-    Visitor.printList(baseNodes);
+    //Visitor.printList(baseNodes);
 
   }
 }
